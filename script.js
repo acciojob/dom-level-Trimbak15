669f8494ddbbbd09d42880e7aa1716428b50a826
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-02 13:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the element you want to find the level of
+var element = document.getElementById('level');
+
+// Initialize the level to 0
+var level = 0;
+
+// Traverse up the DOM tree until you reach the body element
+while (element !== document.body) {
+  element = element.parentNode;
+  level++;
+}
+
+// Display the level in an alert message
+alert('The level of the element is: ' + level);
